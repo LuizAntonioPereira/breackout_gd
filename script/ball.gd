41041db9df184838d64a97e92767021a846d7376
Luiz Antonio <luizantoniodragao@hotmail.com>
2022-07-04 12:38:46 -0300

    up speed

--- script/ball.gd
+++ script/ball.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 
 # Declare member variables here.
-const SPEED := 40.0
+var SPEED := 40.0
 const MAX_SPEED := 1000.0
 var sfx = "res://assets/sons/select_006.ogg"
 const audio_scene = preload("res://scenes/audio_block.tscn")
@@ -13,10 +13,12 @@ func _process(delta):
 	if Global.play == true and yes == true:
 		linear_velocity = Vector2(rand_range(-300, 300), -300)
 		yes = false
+	print(SPEED)
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("plane"):
 		_upgrade_velocity(body)
+		SPEED += 10.0
 
 func _on_body_exited(body: Node) -> void:
 	
@@ -32,7 +34,7 @@ func _upgrade_velocity(body) -> void:
 	
 	var speed = get_linear_velocity().length()
 	var direction = position - body.global_position
-	var velocity = direction.normalized() * min((speed + SPEED) + (10 * Global.point), MAX_SPEED)
+	var velocity = direction.normalized() * min(speed + SPEED, MAX_SPEED)
 	linear_velocity = velocity 
 
 func _on_VisibilityNotifier2D_screen_exited():	
